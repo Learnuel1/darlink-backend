eeb657ac400c8f2a2d1e7b21f02daf8433aeddd4
Learnuel1 <leanueltech@gmail.com>
2023-01-05 19:17:17 +0100

    feature: added plan model

--- src/config/database/script.sql
+++ src/config/database/script.sql
@@ -2,8 +2,8 @@
 CREATE PROC sp_register
 @id varchar(255)
 ,@username varchar(60)
-,@password varchar(255),
-@email varchar(255)
+,@password varchar(255)
+,@email varchar(255)
 ,@role varchar(255)
 ,@plan varchar(40)
 ,@userplanid varchar(255)
@@ -11,13 +11,14 @@ CREATE PROC sp_register
  BEGIN
  BEGIN TRY
  BEGIN TRAN
- DECLARE @default varchar(255)
- SET @default=(SELECT planId FROM tblplan WHERE [plan]=@plan)
-
+ DECLARE @defaultPlanId varchar(255)
+ SET @defaultPlanId=(SELECT planId FROM tblplan WHERE [plan]=@plan)
+  IF @defaultPlanId IS NULL
+  RAISERROR('Registration is not available at the moment',16,1)
  INSERT INTO tblusers(userId,username,password,email,role)
  VALUES(@id,@username,@password,@email,@role)
  
- INSERT INTO tbluserplan(userPlanId,planId,userId) VALUES(@userplanid,@default,@id)
+ INSERT INTO tbluserplan(userPlanId,planId,userId) VALUES(@userplanid,@defaultPlanId,@id)
  COMMIT TRAN
  
  END TRY
@@ -29,5 +30,35 @@ CREATE PROC sp_register
  END CATCH
  END
 
+GO
+CREATE PROC sp_update_plan
+@planId VARCHAR(255)
+,@plans VARCHAR(60)
+,@amount DECIMAL(9,2)
+,@duration VARCHAR(10)
+AS
+BEGIN
+BEGIN TRY
+ 
+UPDATE tblplan SET [plan]=@plans WHERE planId=@planId;
+IF @amount is NOT NULL OR @duration !=' ' 
+UPDATE tblplan SET amount=@amount WHERE planId=@planId
+IF @duration is NOT NULL OR @duration !=' '
+UPDATE tblplan SET duration=@duration WHERE planId=@planId
+UPDATE tblplan SET updatedAt=GETDATE() WHERE planId=@planId
 
-SELECT* from tblplan
+END TRY
+BEGIN CATCH
+DECLARE @em VARCHAR(150)
+SET @em = ERROR_MESSAGE()
+RAISERROR(@em,16,1)
+END CATCH
+END 
+
+SELECT* from tbluserplan
+
+SELECT * FROM tblusers u LEFT JOIN tbluserplan p ON u.userId=p.userId
+
+delete from tblusers
+select * FROM tblplan
+select * FROM tblprofile
--- src/utils/actions.js
+++ src/utils/actions.js
@@ -2,4 +2,5 @@ exports.ACTIONS ={
     ADMIN:"admin",
     JWT_EXPIRED:'jwt expired',
     DEFAULT_PLAN:'Beginner',
+    MESSAGE:'msg',
 }

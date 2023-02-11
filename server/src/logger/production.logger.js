f42f416b984d79de6ab8417928184e2d77b31d3a
Learnuel Linus <learnueltech@gmail.com>
2023-02-11 10:01:56 +0100

    fix: update production logger

--- server/src/logger/production.logger.js
+++ server/src/logger/production.logger.js
@@ -5,35 +5,32 @@ const { combine, timestamp, label, printf, prettyPrint, errors ,json} = format;
  
 exports.proLogger = () => {
  return createLogger({
-  level: 'debug',
   format: combine( 
     timestamp(),
     errors({stack:true}),
     json(),
   ),
   transports: [
+    new transports.MongoDB({
+      level:"infor",
+      db: process.env.ERROR_LOG_URL,
+      options: { useUnifiedTopology: true },
+      collecion:"Darlinkinforlog",
+    }),
     new transports.MongoDB(
       {
     level:"error",
-    collecion: "Darlink_error_log",
     db: process.env.ERROR_LOG_URL,
     options: { useUnifiedTopology: true },
+    collecion: "Darlinkerrorlog",
   }),
+ 
+ new transports.MongoDB({
+  level:"debug",
+  db: process.env.ERROR_LOG_URL,
+  options: { useUnifiedTopology: true },
+  collecion:"Darlinkexceptionlog",
+}),
   ],  
-  transports: [
-    new transports.MongoDB({
-    level:"infor",
-    collecion:"Darlink_infor_log",
-    db: process.env.ERROR_LOG_URL,
-    options: { useUnifiedTopology: true },
-  }),
-  ],  
-  exceptionHandlers: [new transports.MongoDB({
-    level:"debug",
-    collecion:"Darlink_exception_log",
-    db: process.env.ERROR_LOG_URL,
-    options: { useUnifiedTopology: true },
-  }),
-  ]
 })
 } 
